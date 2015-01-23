efa14291046f48a161b2557501f7092e8169a615
Léo-Paul COUTURIER <leopaul3@gmail.com>
2015-01-23 23:20:09 +0100

    MaJ de la doc

--- search/functions_6.js
+++ search/functions_6.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['initbuffer',['initBuffer',['../class_geometry.html#a0e5faf7799502a113886e321cc0e49f0',1,'Geometry']]],
   ['initializegl',['initializeGL',['../class_viewport.html#a747eb6bce25dd9d205f3e5b99817d5cd',1,'Viewport']]],
   ['initprogram',['initProgram',['../class_geometry.html#a56356e0b967bbe1ccc915ec02baa0fa5',1,'Geometry']]]
 ];
