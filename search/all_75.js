a414ab85b25e388c0e8bcbb8e7483dfcecb51624
Travis-CI <travis@leops.me>
2015-01-27 11:19:40 +0000

    Latest docs on successful travis build 127 Commit 35470cdae20b3218b2d43fe125a4c00761066e48

--- search/all_75.js
+++ search/all_75.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['ui',['Ui',['../namespace_ui.html',1,'']]],
-  ['up',['up',['../class_camera.html#a9936c788ad482b40b5a3548bf5e97247',1,'Camera']]]
+  ['up',['up',['../class_camera.html#a9936c788ad482b40b5a3548bf5e97247',1,'Camera']]],
+  ['updatenow',['updateNow',['../class_viewport.html#a24da6de860d5192cd27518cfc2dcb312',1,'Viewport']]]
 ];
--- search/functions_75.js
+++ search/functions_75.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['up',['up',['../class_camera.html#a9936c788ad482b40b5a3548bf5e97247',1,'Camera']]]
+  ['up',['up',['../class_camera.html#a9936c788ad482b40b5a3548bf5e97247',1,'Camera']]],
+  ['updatenow',['updateNow',['../class_viewport.html#a24da6de860d5192cd27518cfc2dcb312',1,'Viewport']]]
 ];
