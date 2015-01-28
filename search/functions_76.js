fe9d4c6d62b1010e350bc0548660a993c2b1bd6d
Travis-CI <travis@leops.me>
2015-01-28 19:08:02 +0000

    Latest docs on successful travis build 140 Commit 762fa1aec0bbb53043b5249b0a04228e46b339fd

--- search/functions_76.js
+++ search/functions_76.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['velocity',['velocity',['../class_camera.html#a2a071a1295b79f2650b01e6d933b1ff2',1,'Camera']]],
   ['view',['view',['../class_camera.html#af81adf3ba7f63429a9d5356c218da78f',1,'Camera']]],
   ['viewport',['Viewport',['../class_viewport.html#a8df3511c07ed30bada06ac942303f668',1,'Viewport']]]
 ];
